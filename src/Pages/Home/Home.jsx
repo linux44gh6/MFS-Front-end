f3af3bb894470677c73f9b2cfa32c5c17ae0eed9
linux44gh6 <abireshan32@gmail.com>
2024-07-17 11:23:43 +0600

    add dashboard

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,53 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>HomePage</div>
+    <div>
+      <div className="navbar bg-primary fixed w-full z-50 px-10 top-0 left-1 border-b">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      
+    </div>
+    <a className="btn btn-ghost text-xl">MFS</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+  </div>
+  <div className="navbar-end">
+  <div className="avatar online">
+  <div className="w-12 rounded-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+</div>
+  </div>
+</div>
+   <div className=' flex justify-between'>
+   <div className=' bg-primary h-[90vh] w-[300px] fixed left-1 mt-8'>
+      
+      <div className=' p-2'>
+      <span className=' text-left text-white font-semibold text-2xl'><h1>My Accout</h1></span>
+      <span className=' text-left text-white font-semibold text-2xl'><h1>My Accout</h1></span>
+      <span className=' text-left text-white font-semibold text-2xl'><h1>My Accout</h1></span>
+      <span className=' text-left text-white font-semibold text-2xl'><h1>My Accout</h1></span>
+      <span className=' text-left text-white font-semibold text-2xl'><h1>My Accout</h1></span>
+      </div>
+     </div>
+     <div className='  bg-orange-100 sticky ml-[272px] mt-4 h-[100vh]  p-4 w-full'>
+    <h1>layout 2</h1>
+     </div>
+   </div>
+    </div>
   )
 }
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import axios from 'axios';
 export default function Register() {
+  
+  const handleToRegister= async e=>{
+     e.preventDefault();
+     const form=e.target
+     const name=form.name.value
+     const number=form.number.value
+     const email=form.email.value
+     const password=form.password.value
+    const role='user'
+    const user={
+      name,email,number,password,role
+    }
+    try {
+      const res = await axios.post((`${import.meta.env.VITE_BASE_URL}/user`),user);
+      alert('Registration successful');
+    } catch (err) {
+      alert('Registration failed');
+    }
+  }
   return (
-    <div className='h-[100vh] '>
-        <div className=' bg-primary h-[100vh] flex justify-between '>
+    <div className='h-[100vh] rounded-lg'>
+        <div className=' bg-primary h-[100vh] flex justify-between rounded-lg'>
        <div className=' w-[500px] p-4'>
         <h1 className=' text-5xl text-left font-extrabold text-white'>MFS</h1>
         <h1 className=' text-left font-bold text-white text-2xl mt-20'>Check the Status</h1>
@@ -20,30 +40,30 @@ export default function Register() {
   <div className="hero-content">
   
     <div className="card  shrink-0 shadow-2xl">
-      <form className="">
+      <form className="" onSubmit={handleToRegister}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="text" placeholder="Your Name" className="input input-bordered " required />
+          <input type="text" name='name' placeholder="Your Name" className="input input-bordered " required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Phone Number</span>
           </label>
-          <input type="number" placeholder="Phone Number" className="input input-bordered" required />
+          <input type="number" name='number' placeholder="Phone Number" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Register</button>
